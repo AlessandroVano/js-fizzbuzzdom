24a6c1f1d9e47c4aa2843d72963b63d6e6f0ac2c
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-25 17:40:04 +0200

    insert fizzbuzz console log

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ const squares = document.querySelector('.squares')
  
 
 for (let i = 1; i <= 100; i++) {
+
     squares.innerHTML += `<div class="square square-${i}">${i}</div>`; 
+//prova
+    if (i % 15 == 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 == 0) {
+        console.log('fizz');
+    } else if (i % 5 == 0) {
+        console.log('buzz');
+    } else {
+        console.log(i);
+    }
 };
 
