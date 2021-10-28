19e94f512246b1a88ccc529f23ee84f0323121ae
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-28 14:37:06 +0200

    insert css style squares fizz, buzz and fizzbuzz, insert generation fizz, buzz and fizzbuzz

--- js/main.js
+++ js/main.js
@@ -10,16 +10,21 @@ const squares = document.querySelector('.squares')
 
 for (let i = 1; i <= 100; i++) {
 
-    squares.innerHTML += `<div class="square square-${i}">${i}</div>`; 
-//prova
-    if (i % 15 == 0) {
+//inserimento generazione fizzbuzz/ fizz / buzz
+     if (i % 15 == 0) {
         console.log('FizzBuzz')
+        element = '<div class="square square-fizzbuzz">fizzbuzz</div>';
     } else if (i % 3 == 0) {
+        element = '<div class="square square-fizz">fizz</div>';
         console.log('fizz');
     } else if (i % 5 == 0) {
+        element = '<div class="square square-buzz">buzz</div>';
         console.log('buzz');
     } else {
+        element =`<div class="square">${i}</div>`;
         console.log(i);
     }
-};
 
+    squares.innerHTML += element;
+};
+ 
